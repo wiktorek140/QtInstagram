80acc92d9ce2a60db397819b18b0b05310e80865
Wiktor Strzębała <wiktorek140@tlen.pl>
2019-12-08 15:26:14 +0100

    update request api

--- src/api2/instagramv2_p.h
+++ src/api2/instagramv2_p.h
@@ -22,7 +22,7 @@ public:
     void loadCookies();
 
     InstagramRequestv2 *fileRequest(QString endpoint, QString boundary, QByteArray data);
-    InstagramRequestv2 *request(QString endpoint, QByteArray post);
+    InstagramRequestv2 *request(QString endpoint, QByteArray post, bool apiV2 = false, isGet = false);
 
 private Q_SLOTS:
     void setUser();
